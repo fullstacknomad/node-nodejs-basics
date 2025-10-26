a56bf6819163214d6a1940840a2179e81bf29dd4
Chandra Behera <fullstacknomad@gmail.com>
2025-10-26 23:52:35 +0530

    feat(cp): implement function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
-const spawnChildProcess = async (args) => {
-  // Write your code here
-};
+import { spawn } from "node:child_process";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const spawnChildProcess = async (args) => {
+  const scriptPath = join(__dirname, "files", "script.js");
+  const childProcess = spawn("node", [scriptPath, ...args]);
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+};
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
